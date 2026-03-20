2ea4a2cfec009d1aa448c2eda6b2adf4944a9939
b1enb1en <leorenzbien@gmail.com>
2026-03-20 09:50:47 +0800

    initial commit

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Details</title>
+    <title>Song Details</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -12,7 +12,7 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        <h3>Student Details</h3>
+                        <h3>Song Details</h3>
                     </div>
                     <div class="card-body">
                         <table class="table table-bordered">
